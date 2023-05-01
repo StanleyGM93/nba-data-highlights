42a072595d2edd2fa0f37b012889a20605ccab9a
Stanley Mair <stanleygmair@gmail.com>
2023-05-01 19:27:45 +1200

    Setting up express framework

--- server/api.js
+++ server/api.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+app.use(cors());
+
+const ApiKey = process.env.X_RAPIDAPI_KEY;
+
+app.listen(3000, () => {
+	console.log("Server listening on port 3000");
+	console.log(ApiKey);
+});
